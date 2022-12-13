b4d78114bfda6123b58de07c88875326ee7f09b2
karthik <karthik_saravanan@trimble.com>
2022-12-13 10:41:21 +0530

    refactor.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import scss from 'rollup-plugin-scss'
 import dts from "rollup-plugin-dts";
+
 
 // Rollup config exports an array of configuration objects.
 // For a library we need two config objects. 
@@ -21,12 +23,34 @@ export default [
         plugins: [
             resolve(),
             commonjs(),
+            scss(),
+            typescript({tsconfig: './tsconfig.json'}),
+        ]
+    },
+    {
+        input: "src/construction/index.tsx",
+        output: [
+            {
+                file:"construction/index.js",
+                format: "esm",
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            scss(),
             typescript({tsconfig: './tsconfig.json'}),
         ]
     },
     {
         input: "src/transportation/index.tsx",
         output: [{file:"transportation/index.d.ts", format: "esm"}],
+        plugins:[dts()]
+    },
+    {
+        input: "src/construction/index.tsx",
+        output: [{file:"construction/index.d.ts", format: "esm"}],
         plugins:[dts()]
     }
 ]
